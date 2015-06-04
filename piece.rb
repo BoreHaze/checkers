8150fab53271f299dc5da697a90d723b90cf468b
Max Lewin <mslewin@gmail.com>
2015-06-04 09:38:13 -0400

    Implement Piece#move_deltas

--- piece.rb
+++ piece.rb
@@ -1,3 +1,30 @@
 class Piece
-  
+
+  UP_DELTAS   = [[-1,-1], [-1, 1]]
+  DOWN_DELTAS = [[ 1,-1], [ 1, 1]]
+
+  def initialize(board, position, color)
+    @color = color
+    @position = position
+    @board = board
+    @king = false
+  end
+
+  def perform_slide
+
+  end
+
+  def perform_jump
+
+  end
+
+  def promote?
+
+  end
+
+  def move_deltas
+
+
+  end
+
 end
