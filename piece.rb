88749b6fcae3f093a791bf9d137425f739898d82
Max Lewin <mslewin@gmail.com>
2015-06-04 09:46:18 -0400

    Piece#perform_slide

--- piece.rb
+++ piece.rb
@@ -3,6 +3,8 @@ class Piece
   UP_DELTAS   = [[-1,-1], [-1, 1]]
   DOWN_DELTAS = [[ 1,-1], [ 1, 1]]
 
+  attr_reader :position, :color
+
   def initialize(board, position, color)
     @color = color
     @position = position
@@ -10,11 +12,25 @@ class Piece
     @king = false
   end
 
-  def perform_slide
+  def perform_slide(pos)
+
+    return false unless move_deltas.any? do |dy, dx|
+      [position[0] + dy, position[1] + dx] == pos
+    end
+
+    return false if @board.out_of_bounds?(pos) || @board.occupied?(pos)
+
+    @board[position] = nil
+    @position        = pos
+    @board[position] = self
+
+    @king = true if promote?
 
   end
 
-  def perform_jump
+  def perform_jump(pos)
+
+
 
   end
 
@@ -23,8 +39,13 @@ class Piece
   end
 
   def move_deltas
+    return UP_DELTAS + DOWN_DELTAS if king?
 
+    @color == :red ? UP_DELTAS : DOWN_DELTAS
+  end
 
+  def king?
+    @king
   end
 
 end
